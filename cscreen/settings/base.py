31b99f1b2b32d7b991198e4a4cc760abbc068026
Samuel Abasah <kobysam13@gmail.com>
2022-07-19 14:01:59 -0400

    csrf fix

--- cscreen/settings/base.py
+++ cscreen/settings/base.py
@@ -126,3 +126,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS=['https://*.coveredscreen.com']
